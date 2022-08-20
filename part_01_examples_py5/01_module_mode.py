14050afc10efe056afeb72d1b669278e911bc393
tabreturn <github@tabreturn.com>
2022-08-20 15:06:39 +1200

    - reorganise fill code

--- part_01_examples_py5/01_module_mode.py
+++ part_01_examples_py5/01_module_mode.py
@@ -12,12 +12,13 @@ def setup():
 
 def draw():
     py5.stroke_weight(3)
-    # face
-    py5.fill(255, 0, 0)
-    py5.fill('#FF0000')
-    py5.fill('#F00')
+    # some different ways to specify a red fill
+    py5.fill(255, 0, 0)    # rgb value for red
+    py5.fill('#FF0000')    # hex value for red
+    py5.fill('#F00')       # shorthand hex value for red
     py5.color_mode(py5.HSB, 360, 100, 100)
-    py5.fill(0, 100, 100)
+    py5.fill(0, 100, 100)  # hsb value for red
+    # face
     py5.circle(py5.width/2, py5.height/2, 300)
     #eyes
     py5.circle(160, 150, py5.random(50, 150))
